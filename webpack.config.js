a645f142aa4a804b3e4a6022a081c64bb0aa25c2
nthhm <numasawa3228@gmail.com>
2021-09-21 02:17:58 +0900

    Creating source maps and optimizing images

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+  devtool: "source-map",
   entry: "./src/javascripts/main.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
@@ -12,15 +14,32 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "> 30%, not dead" }]],
+            },
+          },
+        ],
+      },
+      {
         test: /\.(css|sass|scss)/,
         use: [
           { loader: MiniCssExtractPlugin.loader },
-          { loader: "css-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false,
+            },
+          },
           { loader: "sass-loader" },
         ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         type: "asset/resource",
         generator: {
           filename: "images/[name][ext]",
@@ -33,6 +52,7 @@ module.exports = {
           //     name: "images/[name].[ext]",
           //   },
           // },
+          { loader: "image-webpack-loader" },
         ],
       },
       {
